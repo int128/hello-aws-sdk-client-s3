b6f85d1117e2caca7c05f14ed76a4d2a1598d7ca
Hidetake Iwata <int128@gmail.com>
2022-04-10 18:32:58 +0900

    Use lib-storage.Upload

--- src/run.ts
+++ src/run.ts
@@ -1,14 +1,21 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import { S3Client } from '@aws-sdk/client-s3'
 
 export const run = async () => {
   const client = new S3Client({})
 
-  await client.send(
-    new PutObjectCommand({
+  const upload = new Upload({
+    client,
+    params: {
       Bucket: 'hello-aws-sdk-client-s3',
       Key: 'hello-aws-sdk-client-s3',
-    })
-  )
+      Body: 'test',
+    },
+  })
+  upload.on('httpUploadProgress', (progress) => {
+    console.log(progress)
+  })
+  await upload.done()
 
   return
 }
