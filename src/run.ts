e196942d6820b71717ef2eda5376433c01a8f30a
Hidetake Iwata <int128@gmail.com>
2022-04-10 19:09:57 +0900

    Use glob

--- src/run.ts
+++ src/run.ts
@@ -1,21 +1,25 @@
 import { Upload } from '@aws-sdk/lib-storage'
 import { S3Client } from '@aws-sdk/client-s3'
+import * as glob from 'glob'
+import { createReadStream } from 'fs'
 
 export const run = async () => {
   const client = new S3Client({})
 
-  const upload = new Upload({
-    client,
-    params: {
-      Bucket: 'hello-aws-sdk-client-s3',
-      Key: 'hello-aws-sdk-client-s3',
-      Body: 'test',
-    },
-  })
-  upload.on('httpUploadProgress', (progress) => {
-    console.log(progress)
-  })
-  await upload.done()
+  const matches = glob.sync('node_modules/**/*', { nodir: true })
+  for (const m of matches) {
+    const stream = createReadStream(m)
+    console.info(m)
+    const upload = new Upload({
+      client,
+      params: {
+        Bucket: 'hello-aws-sdk-client-s3',
+        Key: m,
+        Body: stream,
+      },
+    })
+    await upload.done()
+  }
 
   return
 }
